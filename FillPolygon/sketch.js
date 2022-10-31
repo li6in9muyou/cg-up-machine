33b76ebbbcb3ff80dc89f0a5598bb8ffec7f7191
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-31 18:01:01 +0800

    preview texture

--- FillPolygon/sketch.js
+++ FillPolygon/sketch.js
@@ -7,6 +7,30 @@ control_points = [
   [299, 100],
 ];
 
+let fillerText = "";
+
 function drawArray(array) {
   drawLineLoop(array);
 }
+
+const setText = document.getElementById("setText");
+const previewText = document.getElementById("previewText");
+const ctx = previewText.getContext("2d");
+
+function renderFillerText() {
+  console.log(1);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.font = `bold ${ctx.canvas.height * 1.2}px monospace`;
+  ctx.fillStyle = "red";
+  ctx.fillText(fillerText, 0, ctx.canvas.height - 5);
+}
+
+window.addEventListener("load", () => {
+  fillerText = setText.value;
+  renderFillerText();
+});
+
+setText.addEventListener("input", (ev) => {
+  fillerText = ev.target.value;
+  renderFillerText();
+});
