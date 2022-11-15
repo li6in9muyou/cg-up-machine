e18458739167dbb3416268a96a0fc1c09cfd1c9e
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-15 23:18:28 +0800

    refactor: opt-in for color picker support

--- public/config.js
+++ public/config.js
@@ -5,11 +5,4 @@ const pDim = 4;
 const bgColor = 0;
 const nLines = cH / pDim;
 
-const colorPicker = document.getElementById("setColor");
-let drawColor = colorPicker.value;
-colorPicker.addEventListener("change", (ev) => {
-  drawColor = ev.target.value;
-});
-const colorMap = new Map();
-
 let vertices_client_space = [];
