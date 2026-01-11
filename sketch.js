54f998098a82e464cfdadea56ceb9126be5ed69a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2026-01-11 22:56:17 +0800

    wip: setup debug

--- sketch.js
+++ sketch.js
@@ -1,39 +1,39 @@
 const repeatSix = (attr) => [attr, attr, attr, attr, attr, attr];
 
 const element_attributes = [
-  //Near
-  ...repeatSix([255, 255, 255]),
-  //Bottom
-  ...repeatSix([0, 0, 255]),
-  //Far
-  ...repeatSix([0, 255, 255]),
-  //Left
-  ...repeatSix([255, 0, 255]),
-  //Right
-  ...repeatSix([255, 255, 0]),
+  // //Near
+  // ...repeatSix([255, 255, 255]),
+  // //Bottom
+  // ...repeatSix([0, 0, 255]),
+  // //Far
+  // ...repeatSix([0, 255, 255]),
+  // //Left
+  // ...repeatSix([255, 0, 255]),
+  // //Right
+  // ...repeatSix([255, 255, 0]),
   //Top
   ...repeatSix([0, 255, 0]),
 ];
 
 const elements = [
-  //Near
-  [0, 6, 2],
-  [0, 4, 6],
-  //Bottom
-  [0, 2, 1],
-  [1, 2, 3],
-  //Far
-  [7, 5, 3],
-  [3, 5, 1],
-  //Left
-  [5, 4, 1],
-  [1, 4, 0],
-  //Right
-  [6, 7, 2],
-  [2, 7, 3],
+  // //Near
+  // [0, 6, 2],
+  // [0, 4, 6],
+  // //Bottom
+  // [0, 2, 1],
+  // [1, 2, 3],
+  // //Far
+  // [7, 5, 3],
+  // [3, 5, 1],
+  // //Left
+  // [5, 4, 1],
+  // [1, 4, 0],
+  // //Right
+  // [6, 7, 2],
+  // [2, 7, 3],
   //Top
   [5, 7, 4],
-  [4, 7, 6],
+  // [4, 7, 6],
 ].flat();
 
 const vertices_model_space = [
