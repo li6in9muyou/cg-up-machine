18b341238956dd06f390c50474bd9506dba30727
li6in9muyou <libq5520@mails.jlu.edu.cn>
2026-01-15 19:40:51 +0800

    fix: rect missing two pixels

--- DrawPrimitives/DrawRect/sketch.js
+++ DrawPrimitives/DrawRect/sketch.js
@@ -3,18 +3,20 @@ function drawArray(array) {
     return;
   }
   for (let i = 0; i < array.length; i += 2) {
-    const tl = array[i];
+    const [a, b] = array[i];
+    const [c, d] = array[i + 1];
+    const tl = [Math.min(a, c), Math.min(b, d)];
+    const br = [Math.max(a, c), Math.max(b, d)];
     const [tlx, tly] = tl;
-    const br = array[i + 1];
     const [brx, bry] = br;
 
     if (!colorMap.has(i)) {
       colorMap.set(i, `${drawColor}`);
     }
     const color = colorMap.get(i);
-    lineDDA(tlx, tly, tlx, bry, color);
-    lineDDA(tlx, tly, brx, tly, color);
+    lineDDA(tlx, tly, tlx, bry + 1, color);
+    lineDDA(tlx, tly, brx, tly + 1, color);
     lineDDA(brx, bry, tlx, bry, color);
-    lineDDA(brx, bry, brx, tly, color);
+    lineDDA(brx, bry, brx, tly - 1, color);
   }
 }
