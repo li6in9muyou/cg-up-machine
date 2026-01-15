a63d17fdd069b2c1c7e6b153cf14a4d669863858
li6in9muyou <libq5520@mails.jlu.edu.cn>
2026-01-15 17:11:23 +0800

    Revert "ok: remove drawing primitives and filled polygon"
    This reverts commit 18787298ce1e1af02df59a952cd060705585039a.

---
+++ DrawPrimitives/DrawCircle/sketch.js
@@ -0,0 +1,35 @@
+function drawArray(array) {
+  if (array.length === 0) {
+    return;
+  }
+  for (let i = 0; i < array.length; i += 2) {
+    const tl = array[i];
+    const br = array[i + 1];
+    let color = colorMap.get(i);
+    if (color === undefined) {
+      colorMap.set(i, `${drawColor}`);
+      color = drawColor;
+    }
+    drawCircle(tl, br, color);
+  }
+}
+
+function drawCircle(center, pass, color) {
+  const [cx, cy] = center;
+  const [px, py] = pass;
+  setPixel(...center, "red");
+  const radius = Math.sqrt((py - cy) * (py - cy) + (cx - px) * (cx - px));
+  let prev_x = cx + radius;
+  let prev_y = cy;
+  const steps = radius > 20 ? 100 : 40;
+  const step_theta = (2 * Math.PI) / steps;
+  for (let i = 0; i < steps; i++) {
+    const theta = i * step_theta;
+    const next_x = cx + radius * Math.cos(theta);
+    const next_y = cy + radius * Math.sin(theta);
+    lineDDA(prev_x, prev_y, next_x, next_y, color);
+    prev_x = next_x;
+    prev_y = next_y;
+  }
+  lineDDA(prev_x, prev_y, cx + radius, cy, color);
+}
